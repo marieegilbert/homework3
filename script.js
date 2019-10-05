67b1713815763b8ef6d06a797556d12a6a3ae505
Gilbert <Marie.Gilbert@sony.com>
2019-10-05 05:53:06 -0700

    added JS file

---
+++ script.js
@@ -0,0 +1,10 @@
+var generatePassword = document.querySelector("#generate");
+var copyPassword = document.querySelector("#copy");
+
+generatePassword.addEventListener("click", function() {
+    
+  });
+  
+  copyPassword.addEventListener("click", function() {
+
+  }); 
