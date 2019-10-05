b7d261c45c22087c7546c343a60a88fb826ab7ba
Gilbert <Marie.Gilbert@sony.com>
2019-10-05 08:30:33 -0700

    updated javascript

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Generate password //
+function generate(){
+
+    //Set length of password characters//
+    var characterNumber = document.getElementById("slider").value;
+
+    //Setting the password character value options//
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&*+?@^~";
+
+    //Generate the random password//
+    var password = "";
+
+    //for loop to choose password//
+    for [var i = 0; i = characterNumber; i++]{
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
+    }
+    
+    //Display password value in textbox//
+    document.getElementById("password").value = password;
+}
+
 var generatePassword = document.querySelector("#generate");
 var copyPassword = document.querySelector("#copy");
 
